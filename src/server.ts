274a8fa8411e4727cb55bd2651addeeb80886104
Lucas Figueiredo <lucasfigueiredo@MacBook-Pro-de-Lucas.local>
2025-08-11 20:54:04 -0300

    ajustar host para deploy no Render

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app.js'
 import { env } from './env/index.js'
 
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+
 app
   .listen({
     port: env.PORT,
+    host, // <== adiciona aqui
   })
   .then(() => {
     console.log('HTTP Server Running!')
